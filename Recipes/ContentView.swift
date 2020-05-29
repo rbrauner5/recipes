28799c7464e31f3295f444d6047941d26c09f5ce
Rachel Brauner <rachelbrauner@gmail.com>
2020-05-28 19:15:17 -0600

    Initial Commit

---
+++ Recipes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Recipes
+//
+//  Created by Rachel Brauner Buchanan on 5/28/20.
+//  Copyright © 2020 Rachel Brauner Buchanan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
